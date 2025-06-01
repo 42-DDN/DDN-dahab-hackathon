c14680bda1eac15049c21d1a0096c047a29663f0
Proton <basselawni1@gmail.com>
2025-06-01 03:52:09 +0300

    Sessions setup on index.js

--- project-root/backend/index.js
+++ project-root/backend/index.js
@@ -3,6 +3,9 @@ import dotenv from "dotenv";
 import { sellerRouter } from "./routes/sellRoute.js";
 import { adminRouter } from "./routes/loginRoute.js";
 import connectDB from "./config/db.js";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
@@ -10,6 +13,20 @@ const PORT = process.env.PORT;
 connectDB();
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI,
+      collectionName: "sessions",
+    }),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24,
+    },
+  })
+);
 
 app.use("/api/seller", sellerRouter);
 app.use("/api/admin", adminRouter);
