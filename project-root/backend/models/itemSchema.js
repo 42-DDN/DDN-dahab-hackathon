29e1e8599bd90c2b7a58cf0a27cbc03f9faec963
Proton <basselawni1@gmail.com>
2025-06-02 03:25:29 +0300

    hahaha

--- project-root/backend/models/itemSchema.js
+++ project-root/backend/models/itemSchema.js
@@ -12,7 +12,7 @@ const ItemSchema = new mongoose.Schema({
   karat: {
     type: String,
     required: true,
-    enum: ["24k", "21k", "18k", "14k"],
+    enum: ["24k", "21k", "18k", "14k", "24K", "21K", "18K", "14K"], // Accept both cases
   },
   manufacturePrice: {
     type: Number,
