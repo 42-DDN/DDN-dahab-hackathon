dc7ba2879671a6f6bc63e32ca15d1f134a0c65fe
Moe-Salim91156 <hmodaisleem88@gmail.com>
2025-06-02 03:55:21 +0300

    ITS WORKINNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNG

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+KEY_PATH="$HOME/Documents/key1.pem"
+USER="admin"
+HOST="ec2-56-228-19-249.eu-north-1.compute.amazonaws.com"
+GITHUB_REPO="https://github.com/42-DDN/DDN-dahab-hackathon"
+REMOTE_DIR_NAME="DDN-dahab-hackathon-main"
+
+
+
+echo "🔐 Connecting to EC2 and setting up the project..."
+ssh -i "$KEY_PATH" $USER@$HOST << EOF
+  echo "✅ Connected to EC2 : SUCCESS"
+  sudo apt update
+  sudo apt install -y git make docker.io nodejs npm
+  if [ ! -d "$REMOTE_DIR_NAME" ]; then
+    git clone "$GITHUB_REPO"
+  fi
+  cd "$REMOTE_DIR_NAME"
+	echo "RUNNING UP THE DOCKER CONTAINERS"
+	sudo make up
+EOF
+
